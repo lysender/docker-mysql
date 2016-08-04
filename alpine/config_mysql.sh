84453690b572a5687b46434ef97196b5bc924845
Leonel <leonel@lysender.com>
2016-08-04 14:56:20 +0000

    Update alpine build

--- alpine/config_mysql.sh
+++ alpine/config_mysql.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-__mysql_config() {
-    rm -rf /var/lib/mysql
-    mysqld --initialize-insecure=on
-    chown -R mysql:mysql /var/lib/mysql
-}
-
-__mysql_config
+mysql_install_db
 
--- alpine/config_root_account.sh
+++ alpine/config_root_account.sh
@@ -5,7 +5,7 @@ ROOT_PASSWORD=${ROOT_PASSWORD:-foobar}
 __setup_root() {
     echo "Setting up root password."
     /usr/bin/mysqld_safe & sleep 10
-    mysqladmin -u root password $ROOT_PASSWORD
+    /usr/bin/mysqladmin -u root password $ROOT_PASSWORD
     killall mysqld
     sleep 10
 }
